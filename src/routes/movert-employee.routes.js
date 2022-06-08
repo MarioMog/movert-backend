03fc182796cef574c5db5958c903c97bba4472cf
MarioMog <mamg3108@gmail.com>
2022-06-08 13:12:04 -0500

    feat: movert-employee /login /user

---
+++ src/routes/movert-employee.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const { loginEmployee } = require('../controllers/Employee.controller')
+const { getUserById } = require('../controllers/user.controller')
+const { isAuthorized } = require('../middlewares/isAuthorized')
+
+router.post('/login', loginEmployee)
+router.post('/user', isAuthorized, getUserById)
+
+module.exports = router

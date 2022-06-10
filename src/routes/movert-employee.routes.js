138a5382fa0bee0bfbffb9426862431989029658
MarioMog <mamg3108@gmail.com>
2022-06-10 10:33:06 -0500

    feat: movert-employee routes

--- src/routes/movert-employee.routes.js
+++ src/routes/movert-employee.routes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const { loginEmployee } = require('../controllers/employee.controller')
 const { getUserById } = require('../controllers/user.controller')
+const { createCard, associateCardToUser, getCardById } = require('../controllers/card.controller')
 const { isAuthorized } = require('../middlewares/isAuthorized')
 
 router.post('/login', loginEmployee)
 router.post('/user', isAuthorized, getUserById)
+router.put('/card', isAuthorized, createCard)
+router.post('/card', getCardById)
+router.post('/card/user', isAuthorized, associateCardToUser)
 
 module.exports = router

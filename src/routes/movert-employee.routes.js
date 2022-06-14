c2eebaceb29cfbd65c4fc61c937e64b36efb04ec
MarioMog <mamg3108@gmail.com>
2022-06-14 09:27:53 -0500

    feat: routes to manipulate user/card data

--- src/routes/movert-employee.routes.js
+++ src/routes/movert-employee.routes.js
@@ -1,13 +1,20 @@
 const router = require('express').Router()
 const { loginEmployee } = require('../controllers/employee.controller')
-const { getUserById } = require('../controllers/user.controller')
-const { createCard, associateCardToUser, getCardById } = require('../controllers/card.controller')
+const { getUserById, registerUser } = require('../controllers/user.controller')
+const { createCard, associateCardToUser, getCardById, updateCard } = require('../controllers/card.controller')
 const { isAuthorized } = require('../middlewares/isAuthorized')
 
+// Endpoints for EmployeeController
 router.post('/login', loginEmployee)
+
+// Endpoints for UserController
+router.put('/user', isAuthorized, registerUser)
 router.post('/user', isAuthorized, getUserById)
+
+// Endpoints for CardController
 router.put('/card', isAuthorized, createCard)
 router.post('/card', getCardById)
 router.post('/card/user', isAuthorized, associateCardToUser)
+router.post('/card/update', isAuthorized, updateCard)
 
 module.exports = router

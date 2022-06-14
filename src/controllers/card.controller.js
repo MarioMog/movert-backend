e9eaaaf37e94d428025ba9f60d204ae09a691cf8
MarioMog <mamg3108@gmail.com>
2022-06-14 09:26:29 -0500

    feat: updateCard in card controller

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -8,14 +8,13 @@ module.exports.createCard = async (req, res) => {
     const idCard = await uuidv4().replace(/-/g, '').replace(/(.{2})/g, '$1:').substring(0, 23)
     const card = await createCard({ ...body, id_card: idCard })
     if (!card) {
-      res.status(401).json({
+      return res.status(401).json({
         error: 'cannot create card'
       })
-    } else {
-      res.status(200).json({
-        id_card: card.id_card
-      })
     }
+    return res.status(200).json({
+      id_card: card.id_card
+    })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Server internal error', err: error })
@@ -27,24 +26,54 @@ module.exports.associateCardToUser = async (req, res) => {
     const { body } = req
     const card = await getCardByUserId(body.id_user)
     if (card) {
-      res.status(401).json({
+      return res.status(401).json({
         error: 'user alredy has a card associated'
       })
+    }
+    if (body.user_type === 1) {
+      if ('school' in body) body.expiration = Date.now()
+      else {
+        return res.status(400).json({ message: 'school cannot be undefined if user_type is 1' })
+      }
     } else {
-      let errorFlag = false
+      body.expiration = null
+      body.school = null
+    }
+    const [result] = await updateCard(body.id_user, body)
+    res.status(200).json({
+      affected_rows: result
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Server internal error', err: error })
+  }
+}
+
+module.exports.updateCard = async (req, res) => {
+  try {
+    const { body } = req
+    const card = await getCardByUserId(body.id_user)
+    if (!card) {
+      return res.status(401).json({
+        error: 'user dont have a card associated'
+      })
+    } else {
       if (body.user_type === 1) {
-        if ('school' in body) body.expiration = Date.now()
-        else {
-          errorFlag = true
-          res.status(400).json({ message: 'school cannot be undefined if user_type is 1' })
+        if ('school' in body) {
+          const date = new Date()
+          date.setMonth(date.getMonth() + 6)
+          body.expiration = date
+        } else {
+          return res.status(400).json({ message: 'school cannot be undefined if user_type is 1' })
         }
+      } else {
+        body.expiration = null
+        body.school = null
       }
-      if (!errorFlag) {
-        const [result] = await updateCard(body.id_user, body)
-        res.status(200).json({
-          affected_rows: result
-        })
-      }
+      const [result] = await updateCard(body.id_user, body)
+      return res.status(200).json({
+        affected_rows: result
+      })
     }
   } catch (error) {
     console.log(error)
@@ -58,12 +87,11 @@ module.exports.getCardById = async (req, res) => {
     const card = await getCardById(idCard)
 
     if (!card) {
-      res.status(401).json({
+      return res.status(401).json({
         error: 'invalid id_card'
       })
-    } else {
-      res.status(200).json(card)
     }
+    return res.status(200).json(card)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Server internal error', err: error })
